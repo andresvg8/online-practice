5e4667d6bdf08682511dcfab6c6c44ae80f86f8e
andresvg8 <66088651+andresvg8@users.noreply.github.com>
2021-01-06 10:01:33 -0500

    sophos java test mid

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -14,7 +14,25 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("HOLA, ESTE ES UN MENSAJE DE PRUEBA.");
+		int i;
+		int j;
+		for(i=0, j=0; j<1; ++j, i++) {
+			System.out.println(i+" "+j);
+		}
+		System.out.println(i+" "+j);
 		
+		int[] arr = new int[0];
+		System.out.println("Size: "+arr.length);
+		
+		String a="Hello";
+		new TestClass().probe(a);
+		
+		char c="f";
+		int ar[][] = new int[][5];
+		String s = 'b'+new Integer(63)+"a";
+		int []b[]=new int[4][];
+		Integer num=new Integer(5);
 	}
 
 }
---
+++ src/principal/TestClass.java
@@ -0,0 +1,16 @@
+package principal;
+
+public class TestClass {
+
+	public void probe(Integer x) {
+		System.out.println("Hola, soy integer");
+	}
+	
+	public void probe(Object x) {
+		System.out.println("Hola, soy Object");
+	}
+	
+	public void probe(Long x) {
+		System.out.println("Hola, soy Long");
+	}
+}
